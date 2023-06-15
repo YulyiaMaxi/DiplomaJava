a7cf0d088999a4324e9cbb189c683bb356be55f8
YulyiaMaxi <126344764+YulyiaMaxi@users.noreply.github.com>
2023-06-15 17:49:30 +0300

    Update IllegalArgumentException.java
    добавлены исправления в класс IllegalArgumentException

--- src/main/java/ru/netology/javaqadiplom/IllegalArgumentException.java
+++ src/main/java/ru/netology/javaqadiplom/IllegalArgumentException.java
@@ -4,8 +4,8 @@ public class IllegalArgumentException extends RuntimeException {
     public IllegalArgumentException(String msg) {
         super(msg);
     }
-<<<<<<< HEAD
+
 }
-=======
-}
->>>>>>> saving
+
+
+
